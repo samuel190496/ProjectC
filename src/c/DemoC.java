77f8a08d6e0eb719ddc795b4bd2365315b55afbb
Samuel <Samuel@192.168.0.21>
2024-01-03 18:05:46 -0800

    initial commit

---
+++ src/c/DemoC.java
@@ -0,0 +1,11 @@
+package c;
+
+public class DemoC {
+	public static void main(String[] args) {
+		System.out.println("Wednsday");
+		
+		System.out.println("Batch 11 new year");
+		System.out.println("2024 is here ");
+	}
+
+}
